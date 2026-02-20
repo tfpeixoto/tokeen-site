ec43eb670e0d0f1896fe1e4ee72899d1f5bd6943
Thiago Peixoto - Hotmart <thiago.peixoto@hotmart.com>
2026-02-20 18:14:27 -0300

    fix color text and grid news

--- src/components/BlogList/index.tsx
+++ src/components/BlogList/index.tsx
@@ -33,7 +33,7 @@ export default function BlogList({
   return (
     <section className="bloglist">
       <Container>
-        <div className="grid grid-cols-3 gap-6">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {posts.slice(0, visibleCount).map((post: CardPostProps) => (
             <CardPost key={post._id} {...post} />
           ))}
